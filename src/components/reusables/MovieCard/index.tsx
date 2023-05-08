f8569e96ad9778937ff3e76a796feb2f77c7a2cb
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-05-08 16:51:01 +0300

    movie card style upd

--- src/components/reusables/MovieCard/index.tsx
+++ src/components/reusables/MovieCard/index.tsx
@@ -1,7 +1,7 @@
 import { Col } from "react-bootstrap";
 import { IMovie } from "../../../interfaces/IMovie";
 import "./style.css";
-import { BsFillInfoCircleFill, BsStarFill } from "react-icons/bs";
+import { BsStarFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }: props) => {
@@ -9,16 +9,15 @@ const MovieCard = ({ movie }: props) => {
     <Col className="d-flex justify-content-center">
       <div className="movie-card">
         <div className="movie-card-body">
-          <img src={movie.poster} alt="movie cover" className="img-fluid" />
-          <div className="poster-overlay">
-            <span>
-              {movie.imdbRating}
-              <BsStarFill fill="#f5c518" />
-            </span>
-            <Link to={`/movie/${movie._id}`}>
-              <BsFillInfoCircleFill />
-            </Link>
-          </div>
+          <Link to={`/movie/${movie._id}`}>
+            <img src={movie.poster} alt="movie cover" className="img-fluid" />
+            <div className="poster-overlay">
+              <span>
+                {movie.imdbRating}
+                <BsStarFill fill="#f5c518" />
+              </span>
+            </div>
+          </Link>
         </div>
       </div>
     </Col>
